d660705e7e3387e9b007d1c21cc414b6af5ae1bb
Arkadiusz Neuman <arkadiusz.neuman@gmail.com>
2015-11-15 17:25:00 +0100

    Created simple webapi.

--- NodeJS-Server/app.js
+++ NodeJS-Server/app.js
@@ -1,9 +1,44 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var restResponse = require('express-rest-response');
 
+app.use(restResponse({
+  showStatusCode: false,  
+  showDefaultMessage: false  
+}));
+
+app.use('/', express.static(__dirname + '/public'));
+app.use('/', express.static(__dirname + '/bower_components'));
+
+//serve main file
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.sendFile(__dirname + '/public/html/index.html');
 });
+
+//api
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var router = express.Router();
+
+var testbool = true;
+router.get('/device', function(req, res) {
+  res.rest.success({ lamp1: testbool });
+});
+
+router.post('/device/:id', function(req, res) {
+  testbool = !testbool;
+  res.rest.success();
+});
+
+router.get('/device/:id', function(req, res) {
+  var returnObj = {};
+  returnObj[req.params.id] = testbool;
+  res.rest.success(returnObj);
+});
+
+app.use('/api', router);
 
 var server = app.listen(process.env.PORT || 3000, function () {
   var host = server.address().address;
