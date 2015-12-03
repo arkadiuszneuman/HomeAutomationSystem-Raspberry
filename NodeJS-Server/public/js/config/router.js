ec8ab158c7c4cf1735778717146c5a118b90439c
Arkadiusz Neuman <arkadiusz.neuman@gmail.com>
2015-12-03 23:30:00 +0100

    Changed content to router.

--- NodeJS-Server/public/js/config/router.js
+++ NodeJS-Server/public/js/config/router.js
@@ -1,23 +1,38 @@
-homeAutomationApp.run(function($rootScope, $location, $state) {
-    $rootScope.$on('$viewContentLoaded', function() {
-        initjQuery();
-    });
+homeAutomationApp.run(function ($rootScope, $location, $state) {
+  $rootScope.$on('$viewContentLoaded', function () {
+    initjQuery();
+  });
 });
 
 homeAutomationApp.config(function ($stateProvider, $urlRouterProvider) {
 
-  $urlRouterProvider.otherwise("/");
+  $urlRouterProvider.when("", "/receivers");
+  $urlRouterProvider.when("/", "/receivers");
+  $urlRouterProvider.otherwise("/receivers");
 
   $stateProvider
     .state('home', {
-      url: "/",
+      url: "",
       templateUrl: "html/index.html",
+      abstract: true,
       views: {
         'menu': {
           templateUrl: 'html/partials/menu.html',
         },
+        'content': {
+          templateUrl: 'html/partials/content.html',
+        },
       }
     })
 
-;
+    .state('home.receivers', {
+      url: "/receivers",
+      views: {
+        'main@': {
+          templateUrl: 'html/partials/pages/receivers.html',
+          controller: 'mainCtrl'
+        },
+      }
+    })
+  ;
 });
