f5b89fa265cb83ed3bbabc985d84dcb8a5a4c06d
Norbert Rozmus <nrozmus@insolutions.pl>
2016-03-05 16:21:29 +0100

    Clean up index.html

--- NodeJS-Server/public/js/controllers/loginCtrl.js
+++ NodeJS-Server/public/js/controllers/loginCtrl.js
@@ -1,18 +1,20 @@
 /* global PNotify */
-homeAutomationApp.controller('loginCtrl', function ($scope, $http, $state, notify,authService) {
+homeAutomationApp.controller('loginCtrl', function ($scope, $http, $state, notify, authService) {
     $scope.user = { name: '', password: '' };
 
 
 
     $scope.loginUser = function () {
-        authService.Login($scope.user,function (loginResult) {
+        authService.Login($scope.user, function (loginResult) {
 
             if (loginResult.success) {
                 notify.show({
                     title: 'Welcome in Home Automation System',
-                    text: 'piczi',
+                    text: 'login success',
                     type: 'success'
                 });
+
+                $state.go('home.logs');
 
             } else {
                 notify.show({
@@ -24,38 +26,4 @@ homeAutomationApp.controller('loginCtrl', function ($scope, $http, $state, notif
 
         });
     };
-// 
-// 
-//     $scope.loginUser = function () {
-//             userService.logIn($scope.user)
-//             .success(function (data) {
-//                 console.log(data);
-// 
-//                 if (data.success == true) {
-// 
-//                     authService.set('HACToken',data.token);
-//                     authService.IsLoggedIn = true
-//                     
-//                     userService.setUser($scope.user);
-//                     
-//                     $notify.show({
-//                         title: 'Welcome in Home Automation System',
-//                         text: 'piczi',
-//                         type: 'success'
-//                     });
-//                    
-//                     $state.go('home.logs');
-//                 } else {
-//                     
-//                     $notify.show({
-//                         title: 'Login failed',
-//                         text: data.message,
-//                         type: 'error'
-//                     });
-//                 }
-// 
-//             }).error(function (err) {
-//                 console.log(err);
-//             });
-//     };
 });
