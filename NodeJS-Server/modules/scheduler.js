716b04bf2e2fe1b7c15561971abde8b47b84c760
Arkadiusz Neuman <aneuman@insolutions.pl>
2016-03-05 23:56:54 +0100

    Scheduler handling sunrise/sunset now.

--- NodeJS-Server/modules/scheduler.js
+++ NodeJS-Server/modules/scheduler.js
@@ -2,58 +2,111 @@ var NRFSwitch = require('../modules/nrf-switch');
 var logger = require('winston');
 var express = require('express');
 var models = require('../models');
+var weather = require('../modules/weather');
 
 var CronJob = require('cron').CronJob;
 
 var jobs = [];
+var jobsStartedToday = false;
 
 exports.start = function () {
-      logger.info('Staring scheduler');
-      models.Device.find({ active: true }, function (err, devices) {
-            if (err)
-                  logger.info(err);
-            else {
-                  devices.forEach(function (device) {
-                        device.schedule.forEach(function (schedule) {
-                              if (schedule.active) {
-                                    logger.info('Starting job ' + schedule.name);
+    logger.info('Staring scheduler');
+    models.Device.find({ active: true }, function (err, devices) {
+        if (err)
+            logger.info(err);
+        else {
+            weather.getWeather(function (weather) {
+                devices.forEach(function (device) {
+                    
+                    var nrfSwitch = new NRFSwitch(device.rxPipe, device.txPipe);
+                    
+                    device.schedule.forEach(function (schedule) {
+                        if (schedule.active) {
+                            logger.info('Starting job ' + schedule.name);
+                            
+                            switch (schedule.mainType) {
+                                case 'timeofday':
                                     var job = new CronJob(schedule.cron, function () {
-                                          logger.info('Sending status ' + schedule.status + ' by scheduler');
-                                          var nrfSwitch = new NRFSwitch(device.rxPipe, device.txPipe);
-
-                                          nrfSwitch.error(function (err) {
-                                                logger.error('Scheduler [' + schedule.name + ']  err: ' + err);
-                                          });
-
-                                          var statusToSend = schedule.status === true ? '1' : '0';
-                                          nrfSwitch.send('2' + statusToSend, true, function (response) {
-                                                logger.info('Scheduler [' + schedule.name + '] sent status');
-                                          });
+                                        logger.info('Sending status ' + schedule.status + ' by scheduler');
+                                        
+                                        nrfSwitch.error(function (err) {
+                                            logger.error('Scheduler [' + schedule.name + ']  err: ' + err);
+                                        });
+                                        
+                                        var statusToSend = schedule.status === true ? '1' : '0';
+                                        nrfSwitch.send('2' + statusToSend, true, function (response) {
+                                            logger.info('Scheduler [' + schedule.name + '] sent status');
+                                        });
                                     }, null, true);
-
+                                    
                                     jobs.push(job);
-                              }
-                        }, this);
-                  }, this);
-
-                  var job = new CronJob('*/10 * * * *', function () {
-                        logger.info('Keeping nodejs up');
-                  }, null, true);
-
-                  jobs.push(job);
-            }
-
-      });
+                                    break;
+                                case 'sun':
+                                    if (!weather.error) {
+                                        var jobDate;
+                                        
+                                        switch (schedule.subType) {
+                                            case 'sunrise':
+                                                jobDate = weather.sunrise;
+                                                break;
+                                            case 'sunset':
+                                                jobDate = weather.sunset;
+                                                break;
+                                        }
+                                        
+                                        if (jobDate != null) {
+                                            var finalDate = new Date(jobDate.getTime() + schedule.difference * 60000);
+                                            
+                                            var sunriseJob = new CronJob(finalDate, function () {
+                                                logger.info('Sending status ' + schedule.status + ' by scheduler');
+                                                
+                                                nrfSwitch.error(function (err) {
+                                                    logger.error('Scheduler [' + schedule.name + ']  err: ' + err);
+                                                });
+                                                
+                                                var statusToSend = schedule.status === true ? '1' : '0';
+                                                nrfSwitch.send('2' + statusToSend, true, function (response) {
+                                                    logger.info('Scheduler [' + schedule.name + '] sent status');
+                                                });
+                                            }, null, true);
+                                            
+                                            jobs.push(sunriseJob);
+                                            break;
+                                        }
+                                    }
+                            }
+                        }
+                    }, this);
+                }, this);
+            }, this);
+            
+            var job = new CronJob('*/10 * * * *', function () {
+                logger.info('Keeping nodejs up');
+                
+                if (new Date().getHours() === 1) {
+                    if (!jobsStartedToday) {
+                        jobsStartedToday = true;
+                        logger.info('Restarting jobs');
+                        exports.restart();
+                    }
+                } else {
+                    jobsStartedToday = false;
+                }
+            }, null, true);
+            
+            jobs.push(job);
+        }
+    });
 }
 
 exports.stop = function () {
-      logger.info('Stopping scheduler');
-      jobs.forEach(function (job) {
-            job.stop();
-      }, this);
+    logger.info('Stopping scheduler');
+    jobs.forEach(function (job) {
+        job.stop();
+    }, this);
 }
 
 exports.restart = function () {
-      exports.stop();
-      exports.start();
+    exports.stop();
+    exports.start();
 }
--- NodeJS-Server/modules/weather.js
+++ NodeJS-Server/modules/weather.js
@@ -3,29 +3,32 @@ var logger = require('winston');
 
 exports.getWeather = function (cb) {
     
-    convertEpochToDate = function(epoch) {
-        var d = new Date(epoch * 1000); // The 0 there is the key, which sets the date to the epoch
-        var offset = new Date().getTimezoneOffset();
-        d.setMinutes(d.getMinutes() - offset);
-        return d;
-    }
-
-    request('http://api.openweathermap.org/data/2.5/weather?q=Warsaw,PL&APPID=51a324cb14c17d4a48d1ed5d681e0777', function(error, response, body) {
-        if (!error && response.statusCode === 200) {
-            var weather = JSON.parse(body);
-
-            logger.info('Got weather: ' + body);
-
-            cb({
-                error: false,
-                sunrise: convertEpochToDate(weather.sys.sunrise),
-                sunset: convertEpochToDate(weather.sys.sunset)
-            });
-        } else {
-            logger.error('Erron in weather request. Response: ' + response);
-            cb({
-                error: true
-            });
+    if (cb != null) {
+        var convertEpochToDate = function (epoch) {
+            var d = new Date(epoch * 1000); // The 0 there is the key, which sets the date to the epoch
+            var offset = new Date().getTimezoneOffset();
+            d.setMinutes(d.getMinutes() - offset);
+            return d;
         }
-    });
+        
+        request('http://api.openweathermap.org/data/2.5/weather?q=Warsaw,PL&APPID=51a324cb14c17d4a48d1ed5d681e0777',
+          function (error, response, body) {
+            if (!error && response.statusCode === 200) {
+                var weather = JSON.parse(body);
+                
+                logger.info('Got weather: ' + body);
+                
+                cb({
+                    error: false,
+                    sunrise: convertEpochToDate(weather.sys.sunrise),
+                    sunset: convertEpochToDate(weather.sys.sunset)
+                });
+            } else {
+                logger.error('Erron in weather request. Response: ' + response);
+                cb({
+                    error: true
+                });
+            }
+        });
+    }
 }
