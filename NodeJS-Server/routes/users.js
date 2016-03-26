a088ea6caa67b89c31ebdc7f569eb34eb77f7b67
Arkadiusz Neuman <aneuman@insolutions.pl>
2016-03-26 14:41:27 +0100

    Reformatted users.js.

--- NodeJS-Server/routes/users.js
+++ NodeJS-Server/routes/users.js
@@ -4,53 +4,56 @@ var models = require('../models');
 var router = express.Router();
 var User = require('../models/user');
 
-router
-.get('/users', function (req, res) {
+router.get('/users', function (req, res) {
     logger.info('ss');
     User.find({}, function (err, users) {
         if (err) logger.info(err);
-
+        
         res.json(users);
     });
-})
-.get('/users/:id',function(req,res){
+});
 
-   User.findById(req.params.id,function(err,user){
+router.get('/users/:id', function (req, res) {
+    
+    User.findById(req.params.id, function (err, user) {
         if (err) logger.error(err);
-
-        res.json(user);
-   });
-})
-.delete('/users/:id',function(req,res){
-    
-    User.findByIdAndRemove(req.params.id,function(err){
-               
-        if(err) res.json({ success: false, message: err.message });
         
-        res.status(200).send({success:true});
+        res.json(user);
     });
+});
+
+router.delete('/users/:id', function (req, res) {
     
-})
-.post('/users/',function(req,res){
+    User.findByIdAndRemove(req.params.id, function (err) {
+        
+        if (err) res.json({ success: false, message: err.message });
+        
+        res.status(200).send({ success: true });
+    });
+
+});
+
+router.post('/users/', function (req, res) {
     logger.info(JSON.stringify(req.body));
     
-    User.findByIdAndUpdate(req.body._id,req.body,function(err,u){
-        if(err){
+    User.findByIdAndUpdate(req.body._id, req.body, function (err, u) {
+        if (err) {
             logger.error(err);
             res.json({ success: false, message: err.message });
-        }else{
+        } else {
             res.status(200).send({ success: true });
         }
-        
+
     });
-})
-.put('/users/',function(req,res){
-   
+});
+
+router.put('/users/', function (req, res) {
+    
     logger.info(JSON.stringify(req.body));
     
     User.find({ email: req.body.email }, function (err, users) {
         if (err) logger.info(err);
-
+        
         if (users.length > 0) {
             res.json({ success: false, message: 'There is user with same email' });
         } else {
