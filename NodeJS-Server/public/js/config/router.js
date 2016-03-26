990d729b21722714bb21740112eb8949d251a5d5
Arkadiusz Neuman <aneuman@insolutions.pl>
2016-03-26 15:05:32 +0100

    Reformated router.js.

--- NodeJS-Server/public/js/config/router.js
+++ NodeJS-Server/public/js/config/router.js
@@ -1,112 +1,112 @@
 homeAutomationApp.run(function ($rootScope, $location, $state) {
-  $rootScope.$on('$viewContentLoaded', function () {
-    initjQuery();
-  });
+    $rootScope.$on('$viewContentLoaded', function () {
+        initjQuery();
+    });
 });
 
 homeAutomationApp.config(function ($stateProvider, $urlRouterProvider) {
-
-  $urlRouterProvider.when("", "/receivers");
-  $urlRouterProvider.when("/", "/receivers");
-  $urlRouterProvider.otherwise("/receivers");
-
-  $stateProvider
+    
+    $urlRouterProvider.when("", "/receivers");
+    $urlRouterProvider.when("/", "/receivers");
+    $urlRouterProvider.otherwise("/receivers");
+    
+    $stateProvider
     .state('home', {
-      url: "",
-      templateUrl: "html/index.html",
-      abstract: true,
-      views: {
-        'menu': {
-          templateUrl: 'html/partials/menu.html',
-          controller: 'mainCtrl'
-        },
-        'content': {
-          templateUrl: 'html/partials/content.html',
-          controller: 'mainCtrl'
-        },
-      }
+        url: "",
+        templateUrl: "html/index.html",
+        abstract: true,
+        views: {
+            'menu': {
+                templateUrl: 'html/partials/menu.html',
+                controller: 'mainCtrl'
+            },
+            'content': {
+                templateUrl: 'html/partials/content.html',
+                controller: 'mainCtrl'
+            },
+        }
     })
 
     .state('home.receivers', {
-      url: "/receivers",
-      views: {
-        'main@': {
-          templateUrl: 'html/partials/pages/receivers/receivers.html',
-          controller: 'mainCtrl'
-        },
-      }
+        url: "/receivers",
+        views: {
+            'main@': {
+                templateUrl: 'html/partials/pages/receivers/receivers.html',
+                controller: 'mainCtrl'
+            },
+        }
     })
     .state('home.receivers.add', {
-      url: "/add",
-      views: {
-        'main@': {
-          templateUrl: 'html/partials/pages/receivers/addDevice.html',
-          controller: 'addDeviceCtrl'
-        },
-      }
+        url: "/add",
+        views: {
+            'main@': {
+                templateUrl: 'html/partials/pages/receivers/addDevice.html',
+                controller: 'addDeviceCtrl'
+            },
+        }
     })
     .state('home.receivers.schedule', {
-      url: "/:id/schedule",
-      views: {
-        'main@': {
-          templateUrl: 'html/partials/pages/receivers/schedule.html',
-          controller: 'scheduleCtrl'
-        },
-      }
+        url: "/:id/schedule",
+        views: {
+            'main@': {
+                templateUrl: 'html/partials/pages/receivers/schedule.html',
+                controller: 'scheduleCtrl'
+            },
+        }
     })
       .state('home.receivers.schedule.timeofday', {
         url: '',
         templateUrl: 'html/partials/pages/receivers/scheduletypes/timeofday.html'
-      })
+    })
       .state('home.receivers.schedule.sun', {
         url: '',
         templateUrl: 'html/partials/pages/receivers/scheduletypes/sun.html'
     })
     
     .state('home.logs', {
-      url: "/logs",
-      views: {
-        'main@': {
-          templateUrl: 'html/partials/pages/logs/logs.html',
-          controller: 'logsCtrl'
-        },
-      }
+        url: "/logs",
+        views: {
+            'main@': {
+                templateUrl: 'html/partials/pages/logs/logs.html',
+                controller: 'logsCtrl'
+            },
+        }
     })
-    .state('home.login',{
-         url: "/login",
-      views: {
-        'main@': {
-          templateUrl: 'html/Login/login.html',
-          controller: 'loginCtrl'
-        },
-      }
+    .state('home.login', {
+        url: "/login",
+        views: {
+            'main@': {
+                templateUrl: 'html/Login/login.html',
+                controller: 'loginCtrl'
+            },
+        }
     })
-    .state('home.register',{
-         url: "/register",
-      views: {
-        'main@': {
-          templateUrl: 'html/Login/register.html',
-          controller: 'registerCtrl'
-        },
-      }
+    .state('home.register', {
+        url: "/register",
+        views: {
+            'main@': {
+                templateUrl: 'html/Login/register.html',
+                controller: 'registerCtrl'
+            },
+        }
     })
-    .state('home.profile',{
-         url: "/profile",
-      views: {
-        'main@': {
-          templateUrl: 'html/partials/pages/profile/profile.html',
-          controller: 'profileCtrl'
-        },
-      }
+    .state('home.profile', {
+        url: "/profile",
+        views: {
+            'main@': {
+                templateUrl: 'html/partials/pages/profile/profile.html',
+                controller: 'profileCtrl'
+            },
+        }
     })
-    .state('home.users',{
-         url: "/users",
-      views: {
-        'main@': {
-          templateUrl: 'html/partials/pages/user/usersManagerment.html',
-          controller: 'usersManagermentCtrl'
-        },
-      }
+    .state('home.users', {
+        url: "/users",
+        views: {
+            'main@': {
+                templateUrl: 'html/partials/pages/user/usersManagerment.html',
+                controller: 'usersManagermentCtrl'
+            },
+        }
     })
-  ;
+    ;
 });
